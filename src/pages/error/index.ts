786b0fd93aca598b5aef9143bf901431a423b21c
alex karpovich <aakarpovich@mtt.ru>
2025-02-18 12:53:15 +0300

    fix files

--- src/pages/error/index.ts
+++ src/pages/error/index.ts
@@ -1,3 +1,2 @@
 import './error.scss';
-
 export { default as ErrorPage } from './error.hbs?raw';
--- src/pages/register/index.ts
+++ src/pages/register/index.ts
@@ -1,3 +1,2 @@
 import './register.scss';
-
 export { default as RegisterPage } from './register.hbs?raw';
